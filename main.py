2cd2f2f142cad3b5379d6eb7fc010c576fdf1317
Mezhnun89 <orudzhalievma@yandex.ru>
2022-10-12 17:54:34 +0300

    commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
-r = requests.get('')
-r.status_code
+def save_from_www(link):
+    filename = link.split('/')[-1]
+    print(filename)
+    r = requests.get(link, allow_redirects = True)
+    open(filename, 'wb').write(r.content)
 
+
+link = 'https://www.nalog.gov.ru/html/sites/www.new.nalog.ru/docs/sprav/tnved/TNVED.ZIP'
+
+save_from_www(link)
